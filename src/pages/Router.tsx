8d49c3ec020433c1bf53508d519e8f670e23f23b
DanishSjjd <danishsjjad@gmail.com>
2022-12-22 16:32:55 +0500

    fix(router): stop animation on route change to /product

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,5 +1,4 @@
 import { AnimatePresence } from "framer-motion";
-import { useId } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 
 import Home from "./Home";
@@ -7,12 +6,16 @@ import Product from "./Product";
 import Transition from "./Transition";
 
 const Router = () => {
-  const id = useId();
   const location = useLocation();
+
+  const pathname =
+    location.pathname === "/" || location.pathname === "/product"
+      ? "root"
+      : location.pathname;
 
   return (
     <AnimatePresence mode="wait" initial={false}>
-      <Routes location={location} key={location.pathname}>
+      <Routes location={location} key={pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
         <Route path="/transition" element={<Transition />} />
